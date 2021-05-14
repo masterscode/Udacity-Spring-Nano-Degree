3cb2c145650f253efed8050a9b45b2b550cb1faf
masterscode <ogbinakaemmanuel@gmail.com>
2021-05-14 15:40:56 +0100

    ...

--- src/main/java/social/repositories/Delivery.java
+++ src/main/java/social/repositories/Delivery.java
@@ -1,33 +1,38 @@
-//package social.repository;
-//
-//import lombok.Data;
-//import org.hibernate.annotations.Nationalized;
-//import org.hibernate.annotations.Type;
-//import org.springframework.data.annotation.Id;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import java.time.LocalDateTime;
-//
-//@Data
-//@Entity
-//public class Delivery {
+package social.repository;
+
+import lombok.Data;
+import org.hibernate.annotations.Nationalized;
+import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Delivery {
 //    @javax.persistence.Id
-//    @GeneratedValue
-//    @Id
-//    private Long id;
-//
-//    @Nationalized
-//    private String recipientName;
-//
-//    @Column
-//    private String address;
-//
-//    @Column
-//    private LocalDateTime deliveryTime;
-//
-//    @Type(type="yes_no")
-//    private Boolean completed;
-//
-//}
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @Nationalized
+    private String recipientName;
+
+    @Column
+    private String address;
+
+    @Column
+    private LocalDateTime deliveryTime;
+
+    @Type(type="yes_no")
+    private Boolean completed;
+
+    private Boolean returned;
+
+    @OneToOne
+    private social.repository.Flower product;
+
+}
--- src/main/java/social/repositories/Flower.java
+++ src/main/java/social/repositories/Flower.java
@@ -1,28 +1,31 @@
-//package social.repository;
-//
-//import lombok.Data;
-//import org.hibernate.annotations.Generated;
-//import org.hibernate.annotations.Nationalized;
-//import org.hibernate.annotations.Table;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.Id;
-//import java.math.BigDecimal;
-//
-//@Entity
-//@Data
-////@Table(appliesTo = "plant") // not really sure if there is a name props
-//public class Flower {
-//    @Id
-//    @GeneratedValue
-//    private Long id;
-//
-//    @Nationalized
-//    private String name;
-//
-//    @Column(precision = 12, scale = 4)
-//    private BigDecimal price;
-//
-//}
+package social.repository;
+
+import lombok.Data;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.Nationalized;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+//@Table(appliesTo = "plant") // only use when you want to map an entity to a different table name
+public class Flower {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Nationalized
+    private String name;
+
+    @Column(precision = 12, scale = 4)
+    private BigDecimal price;
+
+    @OneToOne
+    private social.repository.Delivery delivery;
+
+}
